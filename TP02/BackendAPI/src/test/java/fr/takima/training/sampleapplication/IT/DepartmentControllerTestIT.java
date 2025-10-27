889e6c01b4e9204607775624a8c476231724105b
Lem0n10 <lowlemonvx@gmail.com>
2025-10-27 09:20:38 +0100

    main.yml v4

--- TP02/BackendAPI/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ TP02/BackendAPI/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -1,4 +1,5 @@
 package fr.takima.training.sampleapplication.IT;
+
 import fr.takima.training.simpleapi.SimpleApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
